8f70261ad98b21d5d306f51048d805b9f516923d
J-724R <jjpernian@gmail.com>
2023-04-19 15:28:45 -0400

    Check valid roman number almost done, needs one more check

--- Js-TypeScript/13. Roman_to_integer.ts
+++ Js-TypeScript/13. Roman_to_integer.ts
@@ -7,61 +7,80 @@ const RomanSymbolsValues: any = {
     C: 100,
     D: 500,
     M: 1000,
-}
+};
 
-function romanToInt(string: string): number | void {
+function romanToInt(string: string): number | string {
     const validRomanSymbols = checkRomanSymbolValidity(string);
     const validRomanNumber = checkRomanNumberValidity(string);
     let number = 0;
 
     if (!validRomanSymbols) {
-        return console.log("Invalid roman symbols");
+        return "Invalid roman symbols";
     }
     if (!validRomanNumber) {
-        return console.log("Invalid roman number");
+        return "Invalid roman number";
     }
 
 
     for (let i = 0; i < string.length; i++) {
-        const current = RomanSymbolsValues[string[i]]
-        const next = RomanSymbolsValues[string[i+1]]
+        const current = RomanSymbolsValues[string[i]];
+        const next = RomanSymbolsValues[string[i+1]];
         
         console.log(`Current: ${current} next: ${next}`);
         
         if (current < next){
-            number += next - current
+            number += next - current;
             i++
         }else{
-            number += current
+            number += current;
         }
-    }
+    };
 
-    return number
+    return number;
 };
 
 function checkRomanSymbolValidity(string: string): boolean {
     const regex = /[^IVXLCDM]/g ;// Match unallowed values
     return (string.match(regex)) ? false : true;
-}
+};
 
 function checkRomanNumberValidity(string: string): boolean{
-    
     for (let i = 1; i < string.length-2; i++) {
-        const next = RomanSymbolsValues[string[i+1]]
-        const twoAfter = RomanSymbolsValues[string[i+2]]
-        
-        if  ((string[i] == string[i+1]) && ( next < twoAfter ))
-        {
-            return false;
-        }       
+        if (!checkRules(string[i], string[i+1])) return false      
     };
     return true;
-}
+};
 
+function checkRules(currentChar: string, nextChar: string): boolean{
+    // Rules
+    // I can be placed before V (5) and X (10) to make 4 and 9. 
+    // X can be placed before L (50) and C (100) to make 40 and 90. 
+    // C can be placed before D (500) and M (1000) to make 400 and 900
 
-const testArray = "CXVXIV";
+    if((currentChar == "I") 
+        && ((nextChar == "L" || nextChar == "C" || nextChar == "D" || nextChar == "M"))){
 
-console.log(`Valid Roman Symbols: ${checkRomanSymbolValidity(testArray)}`);
-console.log(`Valid Roman Number: ${checkRomanNumberValidity(testArray)}`);
+        return false;
+    } else if((currentChar == "V") 
+        && ((nextChar == "V" || nextChar == "X" || nextChar == "L" || nextChar == "C" || nextChar == "D" || nextChar == "M"))){
+
+        return false
+    } else if ((currentChar == "X") 
+        && (nextChar == "D" || nextChar == "M")){
+
+        return false;
+    } else if((currentChar == "L") 
+        && ((nextChar == "C" || nextChar == "D" || nextChar == "M"))){
+
+        return false
+    }
+
+    return true;
+};
+
+const testArray = "CXXIV";
+
+// console.log(`Valid Roman Symbols: ${checkRomanSymbolValidity(testArray)}`);
+// console.log(`Valid Roman Number: ${checkRomanNumberValidity(testArray)}`);
 
 console.log(romanToInt(testArray))
