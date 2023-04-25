98ed703fa1a3b4e87778693a1e3725a5ac700f20
J-724R <jjpernian@gmail.com>
2023-04-25 00:05:56 -0400

    Ts solution from another user

--- StudyPlans/DataStructure_I/53.MaximumSubarray..py
+++ StudyPlans/DataStructure_I/53.MaximumSubarray..py
@@ -6,9 +6,18 @@ class Solution:
 
     for i in range(len(nums)):
       currentSum = 0
-      for j in range(len(nums)):
+      for j in range(i, len(nums)):
         currentSum += nums[j]
         maxSum = max(maxSum, currentSum)
   
     return maxSum
 
+# Dynamic Programing | Memoization
+class SolutionII:
+    def maxSubArray(self, nums):
+        dp = [[0]*len(nums) for i in range(2)]
+        dp[0][0], dp[1][0] = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            dp[1][i] = max(nums[i], nums[i] + dp[1][i-1])
+            dp[0][i] = max(dp[0][i-1], dp[1][i])
+        return dp[0][-1]
--- StudyPlans/DataStructure_I/53.MaximumSubarray.ts
+++ StudyPlans/DataStructure_I/53.MaximumSubarray.ts
@@ -1,16 +1,32 @@
 // Brute Force | Trying out every possible sub-array in nums
-function maxSubArray(nums: number[]): number {
-  let maxSum = -Infinity
+// Time exceeded 
+// function maxSubArray(nums: number[]): number {
+//   let maxSum = -Infinity
 
-  for (let i = 0; i < nums.length; i++) {
-    let currentSum = 0
-      for (let j = i; j < nums.length; j++) {
-        currentSum += nums[j] 
-        maxSum = Math.max(maxSum, currentSum)
-      }
+//   for (let i = 0; i < nums.length; i++) {
+//     let currentSum = 0
+//       for (let j = i; j < nums.length; j++) {
+//         currentSum += nums[j] 
+//         maxSum = Math.max(maxSum, currentSum)
+//       }
+//   }
+
+//   return maxSum
+// };
+
+// console.log(maxSubArray([1,2.-7,5,-3,6,1,3,-100]));
+
+// Dynamic Programing | Tabulation
+function maxSubArray(nums: number[]): number {
+  let local = 0
+  let global = -Infinity
+
+  for (const num of nums) {
+    local = Math.max(num, local + num)
+    if (local > global) { global = local }
   }
 
-  return maxSum
-};
+  return global
+}
 
 console.log(maxSubArray([1,2.-7,5,-3,6,1,3,-100]));
