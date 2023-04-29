9a7ab073ab2552e3bbcb726e0b98784ad96dd004
J-724R <jjpernian@gmail.com>
2023-04-28 23:21:56 -0400

    name updted

--- StudyPlans/DataStructure_I/118.PascalTriangle.py
+++ StudyPlans/DataStructure_I/118.PascalTriangle.py
@@ -1,8 +1,11 @@
 class Solution:
   def generate(numRows: int):
-    l=[1]*numRows
+    pascal=[1]*numRows
     for i in range(numRows):
-      l[i]=[1]*(i+1)
+      pascal[i]=[1]*(i+1)
       for j in range(1,i):
-        l[i][j]=l[i-1][j-1]+l[i-1][j]
-    return l
+        pascal[i][j]=pascal[i-1][j-1]+pascal[i-1][j]
+    return pascal
+  
+
+
