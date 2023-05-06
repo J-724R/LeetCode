dbbfe974a6bb78dc08c0919a50924a7a6270a082
J-724R <jjpernian@gmail.com>
2023-05-06 12:23:25 -0400

    better names

--- StudyPlans/DataStructure_I/235.LowestCommnAncestorOfABST.py
+++ StudyPlans/DataStructure_I/235.LowestCommnAncestorOfABST.py
@@ -13,16 +13,16 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-      right = max(p.val, q.val)
-      left = min(p.val, q.val)
-      if root.val < left:
-          return self.lowestCommonAncestor(root.right, p, q)
-      elif root.val > right:
-          return self.lowestCommonAncestor(root.left, p, q)
-      else:
-          return root
-
-
+        small = min(p.val, q.val)
+        large = max(p.val, q.val)
+        while root:
+            if root.val > large:  # p, q belong to the left subtree
+                root = root.left
+            elif root.val < small:  # p, q belong to the right subtree
+                root = root.right
+            else:  # Now, small <= root.val <= large -> This is the LCA between p and q
+                return root
+        return None
 
 
 
