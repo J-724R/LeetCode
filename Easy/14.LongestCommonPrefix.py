157a8fb219af3f591228fec2a6dfaf4ac05d1711
J-724R <jjpernian@gmail.com>
2023-05-08 12:53:56 -0400

    typo fixed

--- Easy/14.LongestCommonPrefix.py
+++ Easy/14.LongestCommonPrefix.py
@@ -1,12 +1,12 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs):
       res = ""
 
       for i in range(len(strs[0])):
-        for str in strs:
-          if i == len[str] or str[i] != strs[0][i]:
+        for s in strs:
+          if i == len(s) or s[i] != strs[0][i]:
             return res
-        res += str[0][i]
+        res += strs[0][i]
 
       return res 
 
