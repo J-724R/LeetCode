f12e9ec1da12ec92baa80161913f48719c1e5588
J-724R <jjpernian@gmail.com>
2023-05-13 18:32:21 -0400

    solved

---
+++ Easy/28.FindTheIndexOfTheFirstOccurrenceInAString.py
@@ -0,0 +1,7 @@
+# Fastest solution found
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+      m,n=len(haystack),len(needle)
+      for i in range(m):
+          if haystack[i:i+n]==needle: return i
+      return -1
