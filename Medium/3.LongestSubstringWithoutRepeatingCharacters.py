931e71c12c20ce752411d9d757afa75e9835edd8
J-724R <jjpernian@gmail.com>
2023-05-18 14:07:02 -0400

    intuition

---
+++ Medium/3.LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,8 @@
+# intuition
+
+# crate hashtable with all the char and set them to 0
+# use two pointers one for string and one for counter
+# loop trough string 
+#     check frequency, if char is in substring reset to "" 
+#     add char to subtring 
+# return substring and len(subtring)
