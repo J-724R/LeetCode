181f17e74bb28ab50ced9734c567c9f793d21f00
J-724R <jjpernian@gmail.com>
2023-05-19 13:13:38 -0400

    another solution by orbuluh

--- Medium/22.GenerateParentheses.py
+++ Medium/22.GenerateParentheses.py
@@ -9,3 +9,25 @@ class Solution:
                         ans[k].append("(" + left + ")" + right)
         
         return ans[-1]
+    
+
+class SolutionII:
+# @param {integer} n
+# @return {string[]}
+    def generateParenthesis(self, n):
+        if not n:
+            return []
+        left, right, ans = n, n, []
+        self.dfs(left,right, ans, "")
+        return ans
+
+    def dfs(self, left, right, ans, string):
+        if right < left:
+            return
+        if not left and not right:
+            ans.append(string)
+            return
+        if left:
+            self.dfs(left-1, right, ans, string + "(")
+        if right:
+            self.dfs(left, right-1, ans, string + ")")
