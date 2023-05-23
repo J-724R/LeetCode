70054a44851c767cb72ca7471e0a73d5fb0358f6
J-724R <jjpernian@gmail.com>
2023-05-23 08:10:50 -0400

    extra solution with count()

--- Easy/2011.FinalValueOfVariableAfterPerformingOperations.py
+++ Easy/2011.FinalValueOfVariableAfterPerformingOperations.py
@@ -8,3 +8,12 @@ class Solution:
             if op == "++X" or op == "X++":
                 x += 1
         return x
+    
+
+# with built in count()
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        A=operations.count("++X") 
+        B=operations.count("X++")
+        
+        return A+B-(len(operations)-A-B)
