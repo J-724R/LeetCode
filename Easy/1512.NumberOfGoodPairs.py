616611208d67c180b687ab45ed582656dd4bcbcc
J-724R <jjpernian@gmail.com>
2023-05-28 13:44:26 -0400

    Quadratic sequence by Skywalker5423

--- Easy/1512.NumberOfGoodPairs.py
+++ Easy/1512.NumberOfGoodPairs.py
@@ -10,3 +10,18 @@ class Solution:
         return pairs
 
 
+#  Solution with quadratic sequence formula by Skywalker5423
+class Solution:
+	def numIdenticalPairs(self, nums: List[int]) -> int:
+		nums, memo = sorted(nums), {} 
+		for i in range(len(nums)-1): 
+			if nums[i] == nums[i+1]:
+				if nums[i] not in memo:
+					memo[nums[i]] = 1
+				memo[nums[i]] = memo[nums[i]] + 1 
+	
+		answer = 0
+		for n in memo.values(): 
+			answer += (n**2 - n)//2 
+
+		return answer
