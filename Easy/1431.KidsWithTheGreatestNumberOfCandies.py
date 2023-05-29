3b73ecebc674739c977ce38a621bd662c5d2d2d2
J-724R <jjpernian@gmail.com>
2023-05-29 08:23:32 -0400

    add new solution by mybuddy29

--- Easy/1431.KidsWithTheGreatestNumberOfCandies.py
+++ Easy/1431.KidsWithTheGreatestNumberOfCandies.py
@@ -2,3 +2,11 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         max_ = max(candies) - extraCandies
         return [x >= max_ for x in candies]
+    
+
+# Beats 93% by mybuddy29
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = max(candies)
+        return [candy+extraCandies >= maxCandies for candy in candies]
+        
