ff4755f6d6ccb30e03b5686e3e9b4256fde920d6
J-724R <jjpernian@gmail.com>
2023-05-30 08:36:44 -0400

    Solve with dictionary by AnupBs

--- Easy/1678.GoalParserInterpretation.py
+++ Easy/1678.GoalParserInterpretation.py
@@ -2,3 +2,16 @@ class Solution:
     def interpret(self, command: str) -> str:
         return command.replace("()", "o").replace("(al)", "al")
             
+
+# With dictionarry, its not as fast but is more scalable
+class Solution:
+    def interpret(self, s: str) -> str:
+        d = {"(al)":"al", "()":"o","G":"G"}
+        tmp= ""
+        res=""
+        for i in range(len(s)):
+            tmp+=s[i]
+            if(tmp in d):
+                res+=d[tmp]
+                tmp=""
+        return res
