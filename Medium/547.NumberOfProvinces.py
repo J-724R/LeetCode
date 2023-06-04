9b80fcfd7fbadb6be6d7f5c206c8583e8999f364
J-724R <jjpernian@gmail.com>
2023-06-04 14:52:49 -0400

    Add new solution

--- Medium/547.NumberOfProvinces.py
+++ Medium/547.NumberOfProvinces.py
@@ -15,3 +15,22 @@ class Solution:
         for i in range(len(isConnected)):  
             provinces += dfs(isConnected, visited, i)
         return provinces  
+    
+
+# Faster solution by peyman_np, beats 60%
+class Solution:
+    def findCircleNum(self, A):
+        N = len(A)
+        seen = set()
+        def dfs(node):
+            for nei, adj in enumerate(A[node]):
+                if adj and nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
+        
+        ans = 0
+        for i in range(N):
+            if i not in seen:
+                dfs(i)
+                ans += 1
+        return ans 
