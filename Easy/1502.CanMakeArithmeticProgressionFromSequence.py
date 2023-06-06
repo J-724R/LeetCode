b9723121db64069e4d9cd501bf7643a87e2426b3
J-724R <jjpernian@gmail.com>
2023-06-06 08:36:33 -0400

    faster solution by rock, beats 50%

--- Easy/1502.CanMakeArithmeticProgressionFromSequence.py
+++ Easy/1502.CanMakeArithmeticProgressionFromSequence.py
@@ -8,3 +8,9 @@ class Solution:
             if dif != arr[i] - arr[i-1]:
                 return False
         return True
+    
+# faster solution by rock, beats 50%
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        return all(arr[i - 2] - arr[i - 1] == arr[i - 1] - arr[i] for i in range(2, len(arr)))
