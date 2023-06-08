eb6b698d2d826d8f0540ff3bc2e1aec2db0bb445
J-724R <jjpernian@gmail.com>
2023-06-08 08:50:58 -0400

    Solved with Binary search

--- Easy/1351.CountNegativeNUmbersInASortedMatrix.py
+++ Easy/1351.CountNegativeNUmbersInASortedMatrix.py
@@ -6,3 +6,21 @@ class Solution:
                 if n < 0:
                     res += 1
         return res
+    
+
+# Solved with binary Search
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count = 0
+        n = len(grid[0])
+        currRowNegativeIndex = n - 1
+
+        # Iterate on all rows of the matrix one by one.
+        for row in grid:
+            # Decrease 'currRowNegativeIndex' so that it points to current row's last positive element.
+            while currRowNegativeIndex >= 0 and row[currRowNegativeIndex] < 0:
+                currRowNegativeIndex -= 1
+            # 'currRowNegativeIndex' points to the last positive element,
+            # which means 'n - (currRowNegativeIndex + 1)' is the number of all negative elements.
+            count += (n - (currRowNegativeIndex + 1))
+        return count
